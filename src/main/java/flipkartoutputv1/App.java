2db39e86e44483f4638f86911b9b9da73793ae7f
Girish <girish@wipro.com>
2021-10-08 14:04:21 +0530

    committing the changes to the git

---
+++ src/main/java/flipkartoutputv1/App.java
@@ -0,0 +1,13 @@
+package flipkartoutputv1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Good Morning Devops team!" );
+    }
+}
